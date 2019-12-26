8715c048c314962fa455b0b9a646181a716d2d30
a1335234372 <1335234372@qq.com>
2019-12-26 15:26:07 +0800

    首次创建测试类增加语句

--- src/main/java/com/seele/test/TestSeele.java
+++ src/main/java/com/seele/test/TestSeele.java
@@ -3,5 +3,6 @@ package com.seele.test;
 public class TestSeele {
    public  static void   main(String[] args){
         System.out.println("Hello,World");
+       System.out.println("Hello,World");
     }
 }
